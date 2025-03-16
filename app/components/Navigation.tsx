6fc1d4d4f6bdc0faad9a35d66e424908461131ce
NivakaranS <nivakaran@hotmail.com>
2025-03-16 18:26:45 +0530

    Another commit

--- app/components/Navigation.tsx
+++ app/components/Navigation.tsx
@@ -5,9 +5,31 @@ import Bell from '../images/bell.png';
 import Search from '../images/search.png';
 import Link from 'next/link';
 import { useState } from 'react';
+import { Chokokutai } from 'next/font/google';
 
 const Navigation = () => {
     const [search, setSearch] = useState(false);
+
+    const cookies = document.cookie;
+    console.log(cookies)
+
+    const parseCookies = () => {
+        const cookiesObj: { [key: string]: string } = {};
+
+        document.cookie.split(';').forEach(cookie => {
+            const [key, value] = cookie.trim().split('=');
+            cookiesObj[key] = value;
+        })
+
+        return cookiesObj;
+    }
+
+    
+
+    
+    const cookiesObj = parseCookies();
+    console.log(cookiesObj)
+
 
     return (
         <nav className='flex h-[14vh] flex-col fixed top-0 w-full z-50'>
@@ -39,12 +61,17 @@ const Navigation = () => {
                                 </div>
                             </div>
                         </div>
-                        
+
+                        {cookies ?
+                        <div>
+                            <p>{cookies}</p>
+                        </div>
+                        :
                         <Link href="/login">
                             <div className="bg-gray-400 py-[5px] px-[20px] cursor-pointer rounded-full text-black">
                                 <p>Login</p>
                             </div>
-                        </Link>
+                        </Link>}
                     </div>
                 </div>
             </div>
