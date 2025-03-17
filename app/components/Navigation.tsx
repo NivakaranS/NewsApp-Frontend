45da8e9f11b05648e5be9ea978952c75600144f4
NivakaranS <nivakaran@hotmail.com>
2025-03-17 20:52:11 +0530

    Another commit

--- app/components/Navigation.tsx
+++ app/components/Navigation.tsx
@@ -43,7 +43,7 @@ const Navigation = () => {
     const handleLogout = async () => {
 
         try {
-            await axios.get("https://news-app-backend-4rb1.vercel.app/auth/logout", {
+            await axios.post("https://news-app-backend-4rb1.vercel.app/auth/logout", {
                 withCredentials: true
             }).then(response => {
                 console.log(response.data);
