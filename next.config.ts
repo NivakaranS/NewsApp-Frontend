213861599b2ef5ef632d58a3f6fb78b37e778c99
NivakaranS <nivakaran@hotmail.com>
2025-03-20 09:17:02 +0530

    Another commit

--- next.config.ts
+++ next.config.ts
@@ -2,21 +2,16 @@ import type { NextConfig } from "next";
 import { webpack } from "next/dist/compiled/webpack/webpack";
 
 const nextConfig: NextConfig = {
+  /* config options here */
   webpack: (config) => {
-    config.resolve.fallback = { fs: false, net: false, tls: false };
-    return config;
+    config.resolve.fallback = {fs: false, net: false, tls: false}
+    return config
   },
   future: {
     webpack5: true,
   },
-  async rewrites() {
-    return [
-      {
-        source: "/api/:path*",
-        destination: "https://news-app-backend-4rb1.vercel.app/api/:path*", // Proxy API requests
-      },
-    ];
-  },
 };
 
-export default nextConfig; // Use ES6 export for Next.js config
+module.exports = {
+  nextConfig
+}
